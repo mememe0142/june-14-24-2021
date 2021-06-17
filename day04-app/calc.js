1a988126fa5740d3df783aa4358660d4265cfa7a
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-06-17 15:55:53 +0530

    Day04

--- day04-app/calc.js
+++ day04-app/calc.js
@@ -1,5 +1,6 @@
 class Calculator {
     add(a, b) {
+        this.sleep(10);
         return a + b;
     }
 
@@ -13,6 +14,17 @@ class Calculator {
 
     square(num) {
         return num * num;
+    }
+
+    sleep(seconds) {
+        let start = new Date().getTime(),
+        delay = seconds * 1000;
+    
+        while (true) {
+            if ((new Date().getTime() - start) > delay) {
+                break;
+            }
+        }
     }
 }
 module.exports = Calculator;
