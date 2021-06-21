624af4bf05c0c486b2274eac9da5bdf11625b6c6
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2021-06-21 15:12:32 +0530

    Day05

--- day05-app/server.js
+++ day05-app/server.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 
 //app.use(express.json()); //Plugin a json body parser as a middleware
+app.use(express.urlencoded());//Plugin a urlencoded body parser as a middleware
 
 const headerCheck = (req, res, next) => {
     if(req.headers["training"]) {
@@ -21,6 +22,18 @@ app.post("/hello", [express.json()], (req, res) => {
     let { name } = req.body;
     res.end(`Hello ${name}`);
 });
+
+app.post("/bye", (req, res) => {
+    let {name} = req.body;
+    res.end(`Bye Bye ${name}`);
+});
+
+app.get('/temperature/:city', (req, res) => {
+    let {city} = req.params;
+    let temperature = Math.round(Math.random() * 50);
+    res.end(`Temperature of ${city} is ${temperature}`);
+});
+
 
 
 app.listen(8080, () => console.log("server running in 8080"));
